05b544f950ceb2134ba9d625427f29ee45bf63a7
fDynamo <fontaine.dynamo@gmail.com>
2024-04-14 17:34:45 -0700

    start + hero person scroll

---
+++ src/components/HeroPerson/HeroPerson.tsx
@@ -0,0 +1,38 @@
+import styles from "./HeroPerson.module.scss";
+import { Amaranth } from "next/font/google";
+
+const allura = Amaranth({ subsets: ["latin"], weight: "400" });
+
+export default function HeroPerson() {
+  const nameList = [
+    "friends",
+    "partner",
+    "parents",
+    "boyfriend",
+    "best-friend",
+    "mom",
+    "girlfriend",
+    "dad",
+  ];
+
+  const renderNames = () => {
+    const newNameList = [...nameList, ...nameList];
+
+    return newNameList.map((nameStr, i) => {
+      return (
+        <>
+          <h2 className={styles.personText} key={`${i}-name`}>
+            {nameStr}
+          </h2>
+          <div className={styles.spacing} key={`${i}-spacing`}></div>
+        </>
+      );
+    });
+  };
+
+  return (
+    <div className={`${styles.container} ${allura.className}`}>
+      <div className={styles.scrollContainer}>{renderNames()}</div>
+    </div>
+  );
+}
