1b377e817a060ffd0808d1f503ac484c7bfb4c52
fDynamo <fontaine.dynamo@gmail.com>
2024-04-18 16:09:07 -0700

    use API for landing page search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,24 @@
 import styles from "./page.module.scss";
 import { useState } from "react";
 import { ProductObj, FormResponse } from "@/utilities/customTypes";
-import {
-  DUMMY_SEARCH_CONFIG,
-  DUMMY_REC_LIST,
-  DUMM_REC_PRODUCT_MAP,
-} from "@/utilities/dummy";
+import { DUMMY_SEARCH_CONFIG } from "@/utilities/dummy";
 
 import LandingContent from "@/components/content/LandingContent";
 import SearchingContent from "@/components/content/SearchingContent";
+import { fetchRecommend } from "@/utilities/useAPI";
 
 export default function Home() {
-  const [isSearching, setIsSearching] = useState(true);
+  const [isSearching, setIsSearching] = useState(false);
   const [recIdx, setRecIdx] = useState<number>(0);
-  const [recList, setRecList] = useState<string[]>(DUMMY_REC_LIST);
-  const [recProductMap, setRecProductMap] = useState<{
+  const [recList, setRecList] = useState<string[]>([]);
+  const [recProductMap, _setRecProductMap] = useState<{
     [key: string]: ProductObj[];
-  }>(DUMM_REC_PRODUCT_MAP);
+  }>({});
+  function addToRecProductMap(rec: string, productList: ProductObj[]) {
+    const newPM = { ...recProductMap, [rec]: productList };
+    _setRecProductMap(newPM);
+  }
+  const [loadingRec, setLoadingRec] = useState(false);
 
   const currRec = recList[recIdx];
   const currProductList = recProductMap[currRec] || [];
@@ -27,17 +29,51 @@ export default function Home() {
     DUMMY_SEARCH_CONFIG
   );
 
-  function handleSearch(formResponse: FormResponse) {
+  async function handleSearch(formResponse: FormResponse) {
     setSearchFormCache(formResponse);
     setIsSearching(true);
+    const res = await fetchRecommend({
+      formResponse,
+      doProductList: true,
+      doRecList: true,
+    });
+
+    if (res.isError) {
+      // TODO
+      console.error(res.error);
+    } else {
+      setRecList(res.recList);
+      addToRecProductMap(res.productListQuery, res.productList);
+    }
+
+    console.log(res);
   }
 
   function handleSearchingBack() {
     setIsSearching(false);
   }
 
-  function handleRecChange(newRec: string) {
+  async function handleRecChange(newRec: string) {
     const newIdx = recList.indexOf(newRec);
+
+    // See if product list exists
+    if (!recProductMap[newRec]) {
+      const res = await fetchRecommend({
+        doProductList: true,
+        searchKeyWords: newRec,
+        doRecList: false,
+      });
+
+      if (res.isError) {
+        // TODO
+        console.error(res.error);
+      } else {
+        addToRecProductMap(res.productListQuery, res.productList);
+      }
+      setTimeout(() => setRecIdx(newIdx), 1);
+      return;
+    }
+
     setRecIdx(newIdx);
   }
 
