3d7d16fe9528ebb49a6a66df13473647abcbebee
fDynamo <fontaine.dynamo@gmail.com>
2024-04-19 12:31:42 -0700

    loading refactor

--- src/components/content/LoadingContent/LoadingContent.tsx
+++ src/components/content/LoadingContent/LoadingContent.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import styles from "./LoadingContent.module.scss";
 import LoadingSpinner from "@/components/shared/LoadingSpinner/LoadingSpinner";
 import { FormResponse } from "@/utilities/customTypes";
-import { randomIntFromInterval } from "@/utilities/helpers";
+import LoadingText from "@/components/shared/LoadingText";
+import LoadingGif from "@/components/shared/LoadingGif";
 
 const LOAD_STATUS_LIST = [
   "brainstorming recommendations",
@@ -15,98 +16,28 @@ const LOAD_STATUS_LIST = [
   "studying https://en.wikipedia.org/wiki/{WHO}",
 ];
 
-const GIF_LIST = [
-  "https://giphy.com/embed/l3nWhI38IWDofyDrW",
-  "https://giphy.com/embed/TvLuZ00OIADoQ",
-  "https://giphy.com/embed/l2JhORT5IFnj6ioko",
-  "https://giphy.com/embed/iNQ2cIve8rUqI",
-  "https://giphy.com/embed/26Ff6khK7g0TQvSUM",
-  "https://giphy.com/embed/H5Ooe4b04mkawWC8KN",
-  "https://giphy.com/embed/3o7TKILKwQCtphbl7y",
-  "https://giphy.com/embed/l0MYHq0IFikDrVQOc",
-];
-
 const INTERVAL_DELAY = 2500;
 
 type LoadingContentProps = {
   formResponse: FormResponse;
 };
 export default function LoadingContent(props: LoadingContentProps) {
-  const [loadStatusIdx, setLoadStatusIdx] = useState(0);
-  const [gifIdx, setGifIdx] = useState(0);
-
-  const loadStatusHistory = useRef<number[]>([]);
-  const gifHistory = useRef<number[]>([]);
-  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
-
-  useEffect(() => {
-    if (!intervalRef.current) {
-      console.log("doing stuff");
-
-      intervalRef.current = setInterval(() => {
-        setLoadStatusIdx((currIdx) => {
-          if (loadStatusHistory.current.length >= LOAD_STATUS_LIST.length)
-            return currIdx;
-
-          let randomIdx = -1;
-
-          while (true) {
-            randomIdx = randomIntFromInterval(0, LOAD_STATUS_LIST.length);
-            if (randomIdx == currIdx) continue;
-            if (loadStatusHistory.current.includes(randomIdx)) continue;
-            break;
-          }
-
-          const newHistory = [...loadStatusHistory.current, randomIdx];
-          loadStatusHistory.current = newHistory;
-
-          return randomIdx;
-        });
-
-        setGifIdx((currIdx) => {
-          if (gifHistory.current.length >= GIF_LIST.length) return currIdx;
-
-          let randomIdx = -1;
-
-          while (true) {
-            randomIdx = randomIntFromInterval(0, GIF_LIST.length);
-            if (randomIdx == currIdx) continue;
-            if (gifHistory.current.includes(randomIdx)) continue;
-            break;
-          }
-
-          const newHistory = [...gifHistory.current, randomIdx];
-          gifHistory.current = newHistory;
-
-          return randomIdx;
-        });
-      }, INTERVAL_DELAY);
-    }
-
-    return () => {
-      if (intervalRef.current) {
-        console.log("cleaning");
-        clearInterval(intervalRef.current);
-        intervalRef.current = null;
-      }
-    };
-  }, []);
-
-  const renderLoadStatus = () => {
-    let text = LOAD_STATUS_LIST[loadStatusIdx];
-    text = text.replace("{WHO}", props.formResponse.who);
-    return text;
+  const getLoadingTextList = () => {
+    return LOAD_STATUS_LIST.map((text) => {
+      text = text.replace("{WHO}", props.formResponse.who);
+      return text;
+    });
   };
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.statusText}>{renderLoadStatus()}</h1>
+      <LoadingText
+        intervalDelay={INTERVAL_DELAY}
+        textList={getLoadingTextList()}
+        className={styles.statusText}
+      />
       <LoadingSpinner className={styles.spinner} />
-      <iframe
-        src={GIF_LIST[gifIdx]}
-        className={styles.gifEmbed}
-        allowFullScreen
-      ></iframe>
+      <LoadingGif intervalDelay={INTERVAL_DELAY} />
     </div>
   );
 }
---
+++ src/components/shared/LoadingGif/LoadingGif.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./LoadingGif.module.scss";
+import { randomIntFromInterval } from "@/utilities/helpers";
+
+const GIF_LIST = [
+  "https://giphy.com/embed/l3nWhI38IWDofyDrW",
+  "https://giphy.com/embed/cnB7XLeVM9X9BjYB2C",
+  "https://giphy.com/embed/iNQ2cIve8rUqI",
+  "https://giphy.com/embed/26Ff6khK7g0TQvSUM",
+  "https://giphy.com/embed/H5Ooe4b04mkawWC8KN",
+  "https://giphy.com/embed/3o7TKILKwQCtphbl7y",
+  "https://giphy.com/embed/l0MYHq0IFikDrVQOc",
+  "https://giphy.com/embed/JIX9t2j0ZTN9S",
+];
+
+const DEFAULT_INTERVAL_DELAY = 2500;
+
+type LoadingGifProps = {
+  intervalDelay?: number;
+  className?: string;
+};
+export default function LoadingGif(props: LoadingGifProps) {
+  const [gifIdx, setGifIdx] = useState(0);
+
+  const gifHistory = useRef<number[]>([0]);
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  useEffect(() => {
+    if (!intervalRef.current) {
+      intervalRef.current = setInterval(() => {
+        setGifIdx((currIdx) => {
+          if (gifHistory.current.length >= GIF_LIST.length) return currIdx;
+
+          let randomIdx = -1;
+
+          while (true) {
+            randomIdx = randomIntFromInterval(0, GIF_LIST.length);
+            if (randomIdx == currIdx) continue;
+            if (gifHistory.current.includes(randomIdx)) continue;
+            break;
+          }
+
+          const newHistory = [...gifHistory.current, randomIdx];
+          gifHistory.current = newHistory;
+
+          return randomIdx;
+        });
+      }, props.intervalDelay || DEFAULT_INTERVAL_DELAY);
+    }
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <iframe
+        src={GIF_LIST[gifIdx]}
+        className={[styles.gifEmbed, props.className].join(" ")}
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
